0b1cada551cfb01462d9890a289a8151351c19aa
Bolaji Adebayo <badebayo11@gmail.com>
2025-02-19 20:27:42 +0100

    Product slug type updated

--- src/app/(user)/product/[slug]/page.tsx
+++ src/app/(user)/product/[slug]/page.tsx
@@ -1,35 +1,38 @@
+import ProudctInfo from '@/components/ProudctInfo'
 import Container from '@/components/Container'
 import Onsale from '@/components/Onsale'
 import { client, urlFor } from '@/lib/sanityClient'
 import { groq } from 'next-sanity'
 import Image from 'next/image'
-import { ProductProps } from '../../../../../type'
-import ProudctInfo from '@/components/ProudctInfo'
+import { ProductProps } from '../../../../../type' // Ensure this interface exists and is correct
 import { PortableText } from '@portabletext/react'
 import { RichText } from '@/components/RichText'
 
+// Define Props interface correctly for the dynamic page
 interface Props {
   params: {
     slug: string
   }
 }
 
+// Fix generateStaticParams type and ensure it returns the correct data
 export const generateStaticParams = async () => {
   const query = groq`*[_type == 'product']{
-        slug
-    }`
+    slug
+  }`
 
-  const slugs: { slug: { current: string } }[] = await client.fetch(query)
-  const slugRoutes = slugs.map((slug: any) => slug?.slug?.current)
-  return slugRoutes?.map((slug: string) => ({
+  const slugs: { slug: { current: string } }[] = await client.fetch(query) // Ensure we type the response correctly
+  const slugRoutes = slugs.map((slug) => slug?.slug?.current)
+  return slugRoutes.map((slug) => ({
     slug,
   }))
 }
 
 const specialOffersQuery = groq`*[_type == 'product' && position == 'on Sale']{
     ...
-   } | order(_createdAt asc)`
+} | order(_createdAt asc)`
 
+// Single product page component
 const SinglePage = async ({ params: { slug } }: Props) => {
   const query = groq`*[_type == 'product' && slug.current == $slug][0]{
     ...
@@ -54,7 +57,7 @@ const SinglePage = async ({ params: { slug } }: Props) => {
           />
         </div>
         <div className='w-full md:col-span-2 xl:col-span-3 xl:p-14 flex flex-col gap-6 justify-center'>
-          <ProudctInfo product={product} />
+          <ProudctInfo product={product} /> {/* Fixed typo here */}
         </div>
       </div>
       <PortableText value={product?.body} components={RichText} />
